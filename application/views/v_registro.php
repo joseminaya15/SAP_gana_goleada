a0d77e45c477f89341677a4212cdc272757e7d21
joseminaya15 <jose.minayac15@gmail.com>
2018-03-05 12:17:18 -0500

    nuevo registro

--- application/views/v_registro.php
+++ application/views/v_registro.php
@@ -36,11 +36,14 @@
                 </div>
             </div>
             <div class="container">
-                <div class="mdl-card mdl-card-login">
+                <div class="mdl-card mdl-card-login mdl-registro">
                     <div class="mdl-card__title">
                         <img src="<?php echo RUTA_IMG?>logo/logo_login.png">
                     </div>
                     <div class="mdl-card__supporting-text">
+                        <div class="col-xs-12">
+                            <h2>Nuevo registro</h2>
+                        </div>
                         <div class="col-sm-6">
                             <div class="mdl-input">
                                 <input type="text" class="form-control" placeholder="Nombre completo del capit&aacute;n" id="nombre" maxlength="50" onkeypress="return soloLetras(event);">
