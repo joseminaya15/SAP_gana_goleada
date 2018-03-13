ff2b91c2be3671cebb2e6fd57f55fcdcf522d353
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-03-13 18:18:32 -0500

    cambios en las validaciones

--- public/js/nueva_anotacion.js
+++ public/js/nueva_anotacion.js
@@ -59,3 +59,28 @@ function nuevaAnotacion(){
       }
 	});
 }
+function soloLetras(e){
+    key 	   	   = e.keyCode || e.which;
+    tecla 	   	   = String.fromCharCode(key).toLowerCase();
+    letras     	   = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+    especiales 	   = "8-37-39-46";
+    tecla_especial = false
+    for(var i in especiales){
+         if(key == especiales[i]){
+             tecla_especial = true;
+             break;
+         }
+     }
+     if(letras.indexOf(tecla)==-1 && !tecla_especial){
+         return false;
+     }
+ }
+ function valida(e){
+    tecla = (document.all) ? e.keyCode : e.which;
+    if (tecla==8){
+        return true;
+    }
+    patron 		=/[0-9]/;
+    tecla_final = String.fromCharCode(tecla);
+    return patron.test(tecla_final);
+}
