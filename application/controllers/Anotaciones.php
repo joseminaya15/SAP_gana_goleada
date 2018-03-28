f5a61d544fd2c04bd84ce8bae63a1d7a4123d0c3
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-03-28 16:00:32 -0500

    cambios

--- application/controllers/Anotaciones.php
+++ application/controllers/Anotaciones.php
@@ -13,91 +13,91 @@ class Anotaciones extends CI_Controller {
         $this->output->set_header('Pragma: no-cache');
   }
 	public function index(){
-        $data['nombre_capitan'] = $this->session->userdata('Nombre_capitan');
-        $data['nombre_canal']   = $this->session->userdata('Nombre_canal');
-        $goles   = $this->M_datos->getTotalGoles($this->session->userdata('Nombre_canal'));
-        $suma_cn = null;
-        $suma_sc = null;
-        $suma_cl = null;
-        $suma_ca = null;
-        $suma_wb = null;
-        $html    = null;
-        foreach ($goles as $key) {
-            if($key->Id_serv == 1){
-                if($key->Flag == FLAG_APROBADO){
-                    $suma_cn += $key->Goles;
-                    $html .= '<tr>
-                              <td>'.$key->Empresa.'</td>
-                              <td>'.$key->Deal_registration.'</td>
-                              <td>'.$key->Pais.'</td>
-                              <td>'.$key->Flag.'</td>
-                              <td>'.$key->Goles.'</td>
-                            </tr>';
-                }else {
-                    $html .= '<tr>
-                              <td>'.$key->Empresa.'</td>
-                              <td>'.$key->Deal_registration.'</td>
-                              <td>'.$key->Pais.'</td>
-                              <td>'.$key->Flag.'</td>
-                              <td>--</td>
-                            </tr>';
-                }
-            }
-            if($key->Id_serv == 2){
-                if($key->Flag == FLAG_APROBADO){
-                    $suma_sc += $key->Goles;
-                }
-            }
-            if($key->Id_serv == 3){
-                if($key->Flag == FLAG_APROBADO){
-                    $suma_cl += $key->Goles;
-                }
-            }
-            if($key->Id_serv == 4){
-                if($key->Flag == FLAG_APROBADO){
-                    $suma_ca += $key->Goles;
-                }
-            }
-            if($key->Id_serv == 5){
-                if($key->Flag == FLAG_APROBADO){
-                    $suma_wb += $key->Goles;
-                }
-            }
-        }
-        if($suma_cn == null){
-            $suma_cn = 0;
-        }
-        if($suma_sc == null){
-            $suma_sc = 0;
-        }
-        if($suma_cl == null){
-            $suma_cl = 0;
-        }
-        if($suma_ca == null){
-            $suma_ca = 0;
-        }
-        if($suma_wb == null){
-            $suma_wb = 0;
-        }
-        $data['total_cn'] = $suma_cn;
-        $data['total_sc'] = $suma_sc;
-        $data['total_cl'] = $suma_cl;
-        $data['total_ca'] = $suma_ca;
-        $data['total_wb'] = $suma_wb;
-        $data['tabla']    = $html;
+    $data['nombre_capitan'] = $this->session->userdata('Nombre_capitan');
+    $data['nombre_canal']   = $this->session->userdata('Nombre_canal');
+    $goles   = $this->M_datos->getTotalGoles($this->session->userdata('Nombre_canal'));
+    $suma_cn = null;
+    $suma_sc = null;
+    $suma_cl = null;
+    $suma_ca = null;
+    $suma_wb = null;
+    $html    = null;
+    foreach ($goles as $key) {
+      if($key->Id_serv == 1){
+          if($key->Flag == FLAG_APROBADO){
+              $suma_cn += $key->Goles;
+              $html .= '<tr>
+                        <td>'.$key->Empresa.'</td>
+                        <td>'.$key->Deal_registration.'</td>
+                        <td>'.$key->Pais.'</td>
+                        <td>'.$key->Flag.'</td>
+                        <td>'.$key->Goles.'</td>
+                      </tr>';
+          }else {
+              $html .= '<tr>
+                        <td>'.$key->Empresa.'</td>
+                        <td>'.$key->Deal_registration.'</td>
+                        <td>'.$key->Pais.'</td>
+                        <td>'.$key->Flag.'</td>
+                        <td>--</td>
+                      </tr>';
+          }
+      }
+      if($key->Id_serv == 2){
+          if($key->Flag == FLAG_APROBADO){
+              $suma_sc += $key->Goles;
+          }
+      }
+      if($key->Id_serv == 3){
+          if($key->Flag == FLAG_APROBADO){
+              $suma_cl += $key->Goles;
+          }
+      }
+      if($key->Id_serv == 4){
+          if($key->Flag == FLAG_APROBADO){
+              $suma_ca += $key->Goles;
+          }
+      }
+      if($key->Id_serv == 5){
+          if($key->Flag == FLAG_APROBADO){
+              $suma_wb += $key->Goles;
+          }
+      }
+    }
+    if($suma_cn == null){
+        $suma_cn = 0;
+    }
+    if($suma_sc == null){
+        $suma_sc = 0;
+    }
+    if($suma_cl == null){
+        $suma_cl = 0;
+    }
+    if($suma_ca == null){
+        $suma_ca = 0;
+    }
+    if($suma_wb == null){
+        $suma_wb = 0;
+    }
+    $data['total_cn'] = $suma_cn;
+    $data['total_sc'] = $suma_sc;
+    $data['total_cl'] = $suma_cl;
+    $data['total_ca'] = $suma_ca;
+    $data['total_wb'] = $suma_wb;
+    $data['tabla']    = $html;
 		$this->load->view('v_anotaciones', $data);
 	}
   function cerrarCesion(){
-        $data['error'] = EXIT_ERROR;
-        $data['msj']   = null;
-        try {
-            $this->session->unset_userdata('usuario');
-            $this->session->unset_userdata('Id_user');
-            $data['error'] = EXIT_SUCCESS;
-        } catch (Exception $e){
-            $data['msj'] = $e->getMessage();
-        }
-        echo json_encode($data);
+      $data['error'] = EXIT_ERROR;
+      $data['msj']   = null;
+      try {
+          $this->session->unset_userdata('usuario');
+          $this->session->unset_userdata('Id_user');
+          $data['error'] = EXIT_SUCCESS;
+      } catch (Exception $e){
+          $data['msj'] = $e->getMessage();
+      }
+      echo json_encode($data);
     }
     function getDatosAnotaciones(){
        $data['error'] = EXIT_ERROR;
