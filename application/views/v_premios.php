1360008c0c9b6c6ea3f41fb61969e69a85c36ce2
joseminaya15 <jose.minayac15@gmail.com>
2018-04-09 09:16:16 -0500

    premios

--- application/views/v_premios.php
+++ application/views/v_premios.php
@@ -50,9 +50,22 @@
                         <p>Premios para Goleadores SAP</p>
                     </div>
                 </div>
-                <div class="mdl-card mdl-card-ranking m-t-5">
-                    <div class="mdl-card__supporting-text">
-                        <img class="img-premios" src="<?php echo RUTA_IMG?>menu/premios.png">
+                <div class="mdl-card mdl-card-ranking mdl-premios m-t-5">
+                    <div class="mdl-card__supporting-text m-t-10">
+                        <div class="col-sm-4 text-center">
+                            <img src="<?php echo RUTA_IMG?>premios/q2.png">
+                        </div>
+                        <div class="col-sm-4 text-center">
+                            <img src="<?php echo RUTA_IMG?>premios/q3.png">
+                        </div>
+                        <div class="col-sm-4 text-center">
+                            <img src="<?php echo RUTA_IMG?>premios/q4.png">
+                        </div>
+                        <div class="col-xs-12 cierreQ">
+                            <p>Cierres de Q:</p>
+                            <p>Q2: Jun 30/ Q3:Set 30/ Q4: Dic 31</p>
+                            <p>Q: Se presentan los ganadores 15 d&iacute;s luego del cierre</p>
+                        </div>
                     </div>
                 </div>
             </div>
